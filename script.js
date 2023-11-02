deae36cc39850821e7c01a012d9514c75a20f9a8
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-01 19:16:08 -0700

    added the activity instructions to the file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Tonight you will be demonstrating your ability to work with expressions, statements, and variables. Create an HTML page and link an external JavaScript file to it. Inside the JavaScript file:
+
+// Define a variable “myGreeting” and place a string containing a generic greeting inside it.
+// Output the contents of “myGreeting” using console.log(...).
+// Define a constant variable “pi” and place a number containing the value for Pi inside of it.
+// Attempt to re-assign “pi” to something else - what happens? Why?
+// Define a new variable “myNumber” and place a number of your choice inside it.
+// Call the “min” function inside of the “Math” object on both “myNumber” and “pi” - what is the result? Why?
